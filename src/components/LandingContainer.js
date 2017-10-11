e12003a275f7d3071b9550484ea52a64b74b71ac
Neil Steiner <npsteiner@gmail.com>
2017-10-11 13:45:55 -0400

    getting set up

---
+++ src/components/LandingContainer.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+
+class LandingContainer extends React.Component {
+	constructor() {
+		super()
+		this.state = {}
+	}
+
+	render() {
+
+		return null;
+	}
+}
+
+
+
+export default LandingContainer
